e20fbd4444ebc39eb6eb5d5574ae4dc248a3728e
Julián de Pablo <juliandpt98@gmail.com>
2025-05-10 15:44:53 +0200

    feat: creada estructura base

---
+++ src/components/ui/Form.svelte
@@ -0,0 +1,12 @@
+<div data-tf-live="01JTAW5DVYMRQSJGCP54RKN3TH"></div>
+
+<script>
+  import { onMount } from 'svelte';
+
+  onMount(() => {
+    const script = document.createElement('script');
+    script.src = '//embed.typeform.com/next/embed.js';
+    script.async = true;
+    document.body.appendChild(script);
+  });
+</script>
